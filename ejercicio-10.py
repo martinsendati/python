64567e7fce1e0a00c1a6265b0c700f6b30b54868
martinsendati <martin.barrionuevo@sendati.com>
2023-07-28 10:12:17 -0300

    ej 10

---
+++ ejercicio-10.py
@@ -0,0 +1,36 @@
+# Confeccionar una función que reciba un entero y luego imprima la tabla de multiplicar de dicho valor
+# (por ejemplo si recibe un 3 luego debe mostrar del 3 hasta el 30 de 3 en 3)
+
+##def tabla_multiplicar(numero):
+#    # Definimos el rango de múltiplos que queremos mostrar (del 1 al 10 en este caso)
+ #   for i in range(1, 11):
+  #      resultado = numero * i
+   #     print(f"{numero} x {i} = {resultado}")
+
+# Pedimos al usuario que ingrese el número del cual desea ver la tabla
+#try:
+#    num = int(input("Ingresa un número para ver su tabla de multiplicar: "))
+ #   tabla_multiplicar(num)
+#except ValueError:
+ #   print("Debes ingresar un número entero válido.")
+
+#def holaConNombre(name):
+ # print("Hello " + name + "!")
+ 
+#holaConNombre("Ada"
+
+#import random
+
+#numero = random.randit(1,11)
+#print( numer
+
+def multiplicar(x, y):
+    return (x*y) 
+
+num = int(input("Ingresa un número entero: "))
+
+for i in range(0, 11):
+    print("El resultado es:",  multiplicar(num, i))
+
+
+
