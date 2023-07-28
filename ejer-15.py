f78814211ff9b7b2cfab0e94c8a00fbe98d98596
martinsendati <martin.barrionuevo@sendati.com>
2023-07-28 15:38:24 -0300

    int input

--- ejer-15.py
+++ ejer-15.py
@@ -5,7 +5,13 @@ def suma_tupla(tupla):
     return suma
 
 # Definir una tupla con 5 enteros
-tupla_enteros = (10, 20, 30, 40, 50)
+num1 = int(input("Primer numero: ")) 
+num2 = int(input("Segundo numero: "))
+num3 = int(input("Tercero numero: "))
+num4 = int(input("Cuarto numero: "))
+num5 = int(input("Quinto numero: "))
+
+tupla_enteros = (num1, num2, num3, num4, num5)
 
 # Calcular la suma de los elementos de la tupla
 resultado = suma_tupla(tupla_enteros)
